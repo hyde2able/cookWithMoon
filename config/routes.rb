b8593397b75fc4e805f00a40678e7bb432cf3faf
pokohide <hyde31415@gmail.com>
2016-08-28 15:00:58 +0900

    edit route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'materials/index'
+  post '/callback', to: 'webhook#callback'
+  get '/search/:keyword', to: 'webhook#search'
+  get '/images/:rid/:size', to: 'webhook#image'
+  get '/assets/:path/:size', to: 'webhook#assets'
+  get '/tech-img/:tech/:id/:size', to: 'webhook#tech'
 
-  get 'recipes/show'
-
-  get 'recipes/share'
-
-  get 'recipes/tech'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # recipe
+  get '/recipe/:rid', to: 'recipes#show'
+  get '/recipe/:rid/materials', to: 'materials#index'
+  get '/tech/:tech/:id', to: 'recipes#tech'
+  get '/recipe/:rid/share', to: 'recipes#share'
 end
